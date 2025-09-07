d7bab942316c38e18a4142f8459f93a5ec05b889
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-07 19:43:16 +0600

    isso commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,25 +1,9 @@
-"use client";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React from "react";
+import React from 'react'
 
 const About = () => {
-  const router = useRouter();
-  const isLoggedIn = true;
-  const handleNavigation = () => {
-    if (isLoggedIn) {
-      router.push("/about/address");
-    } else {
-      router.push("/");
-    }
-  };
   return (
-    <div>
-      <h2>About Page</h2>
-      <Link href={"/about/address"}>address Page</Link>
-      <button type="button" onClick={handleNavigation}> Address navigate Page</button>
-    </div>
-  );
-};
+    <div>About</div>
+  )
+}
 
-export default About;
+export default About
